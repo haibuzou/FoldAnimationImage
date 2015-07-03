6569252ea8bab87583a75d7d3def5e0be009b4ee
stone <542464069@qq.com>
2015-07-03 18:03:16 +0800

    更新

---
+++ app/src/main/java/com/haibuzou/foldanimationimage/FoldingView.java
@@ -0,0 +1,28 @@
+package com.haibuzou.foldanimationimage;
+
+import android.content.Context;
+import android.graphics.Paint;
+import android.util.AttributeSet;
+import android.view.View;
+
+/**
+ * Created by Administrator on 2015/7/3.
+ */
+public class FoldingView extends View {
+
+    private Paint mpaint;
+
+    public FoldingView(Context context) {
+        super(context);
+        init();
+    }
+
+    public FoldingView(Context context, AttributeSet attrs) {
+        super(context, attrs);
+        init();
+    }
+
+    public void init() {
+
+    }
+}
